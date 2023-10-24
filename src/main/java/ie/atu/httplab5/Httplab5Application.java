30f3bf5d3e28e980ce3bad35601fb7bbb1a54ba4
kennatu23 <g00267173@atu.ie>
2023-10-24 15:39:56 +0100

    Commitlab5

---
+++ src/main/java/ie/atu/httplab5/Httplab5Application.java
@@ -0,0 +1,13 @@
+package ie.atu.httplab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Httplab5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Httplab5Application.class, args);
+    }
+
+}
